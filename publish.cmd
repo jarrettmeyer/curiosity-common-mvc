debd058f6e7cc7011cc142d5968ef240dfe4ab92
Jarrett Meyer <jarrettmeyer@gmail.com>
2011-12-20 06:34:51 -0500

    Updated package references for NuGet v1.6

--- publish.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-msbuild /t:BuildRelease .\build.msbuild
-del *.nupkg
-nuget pack .\Curiosity.Common.Mvc\Curiosity.Common.Mvc.csproj -Prop Configuration=Release
-forfiles /m *.nupkg /c "cmd /c nuget push @file"
